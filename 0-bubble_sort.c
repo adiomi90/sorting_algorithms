02fb19442096ef8170c65bbbeabe1e2fd00da346
adiomi90 <adiomi90@outlook.com>
2023-06-27 17:38:12 -0700

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,6 @@
 #include "sort.h"
 /**
- * bubble_sort - sort array elements from minimum to maximum value
+ * bubble_sort - sort array from min to max
  * @array: array
  * @size: array size
  */
@@ -12,6 +12,7 @@ void bubble_sort(int *array, size_t size)
 	if (size < 2)
 		return;
 	for (i = 0; i < size; i++)
+	{
 		for (index = 0; index < size; index++)
 		{
 			if (array[index] > array[index + 1] && array[index + 1])
@@ -22,4 +23,5 @@ void bubble_sort(int *array, size_t size)
 			print_array(array, size);
 			}
 		}
+	}
 }
